03482138efeaf343fdf8e688afc5ba39368fa2e2
Leomam <axel.froly@gmail.com>
2020-06-21 23:53:42 +0200

    Genames V1.0.3
    hyphens in the email fixed

--- JS/generateName.js
+++ JS/generateName.js
@@ -32,6 +32,7 @@ function getRandomLastName(tabLastname){
 function getRandomEmail(firstname, lastname){
 	item = (firstname + lastname + (Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000) + "@gmail.com").toLowerCase();
 	item = item.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	item = item.replace(/-/g, "");
 	return item;
 }
 
